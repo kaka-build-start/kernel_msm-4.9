2f0f606b5fe0214b8980badc22ea1a11c689a70b
chailu1 <chailu1@lenovo.com>
2016-08-30 15:42:15 +0800

    MMI: phy-msm-usb: configure DCP ICL according to dts
    Configure DCP input current limit according to project dts.
    if project dts not Defenderined this property. DCP ICL is Defenderault
    IDEV_CHG_DCP.
    
    Change-Id: I30b6569150d0b31d41a052ac92de0504b55a6b93
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/893374
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -321,6 +321,10 @@ struct msm_otg {
 	struct work_struct extcon_register_work;
 	struct notifier_block psy_nb;
 	bool enable_sdp_check_timer;
+
+#if IS_ENABLED(CONFIG_MACH_MOTOROLA_MSM8937)
+	unsigned int mmi_chg_dcp_icl;
+#endif
 };
 
 struct ci13xxx_platform_data {
