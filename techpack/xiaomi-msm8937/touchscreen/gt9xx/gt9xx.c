25581756a19a2f7bd9b69bf9e014d6f196b25904
me-cafebabe <me.cafebabe@gmail.com>
2023-04-15 23:57:30 +0800

    mi8937: ts: gt9xx: Add "-mi8937" suffix to driver name
    Change-Id: I601ddabc6088a68616584d4ea15aded4e98d2237

--- techpack/xiaomi-msm8937/touchscreen/gt9xx/gt9xx.c
+++ techpack/xiaomi-msm8937/touchscreen/gt9xx/gt9xx.c
@@ -2609,7 +2609,7 @@ void gtp_esd_off(struct goodix_ts_data *ts)
 
 #ifdef CONFIG_OF
 static const struct of_device_id gtp_match_table[] = {
-	{.compatible = "goodix,gt9xx",},
+	{.compatible = "goodix,gt9xx-mi8937",},
 	{ },
 };
 #endif
